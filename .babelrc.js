c52b414aa347fe72dfe362f81a32dd89701b0b0b
Nikita Gusarov <nickgusarov@gmail.com>
2019-04-19 14:57:36 +0300

    build: fix babel for jest environment

--- .babelrc.js
+++ .babelrc.js
@@ -1,11 +1,15 @@
 'use strict';
 
+const env = process.env.NODE_ENV;
+const isTest = env === 'test';
+
 module.exports = {
   presets: [
     [
       '@babel/preset-env',
       {
         modules: 'commonjs',
+        targets: isTest ? { node: 'current' } : {},
       },
     ],
   ],
