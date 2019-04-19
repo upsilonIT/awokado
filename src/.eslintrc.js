4946d86156a9fb66fc1d21417a5911b5dbbe2065
Nikita Gusarov <nickgusarov@gmail.com>
2019-04-19 11:18:09 +0300

    build: add eslint

---
+++ src/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['@exeto', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['**/*.test.js'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
