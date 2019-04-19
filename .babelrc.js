1d2f5312249fc39eebac39f668b304999d3c3454
Nikita Gusarov <nickgusarov@gmail.com>
2019-04-19 12:42:25 +0300

    build: transform es6 imports to cjs

--- .babelrc.js
+++ .babelrc.js
@@ -2,5 +2,8 @@
 
 module.exports = {
   presets: ['@exeto/babel-preset-react'],
-  plugins: ['@babel/plugin-proposal-class-properties'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-modules-commonjs'
+  ],
 };
