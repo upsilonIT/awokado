128d1a0d6da099a1768a1be78b2d5a36ef950a96
Nikita Gusarov <nickgusarov@gmail.com>
2019-04-26 16:39:48 +0300

    feat(serializers): add top-level module for serializers

---
+++ src/serializers/index.js
@@ -0,0 +1,6 @@
+export { default as applyMiddleware } from "./applyMiddleware";
+export { default as combineMiddlewares } from "./combineMiddlewares";
+export {
+  default as createQuerySerializer,
+  defaultAnnotations
+} from "./createQuerySerializer";
